4c31530f107833d24cbde55bf16d96af7b57ea69
David Oster <paspa@hotmail.com>
2020-04-11 15:35:00 +0300

    Update for BubbleSort

---
+++ src/grocerystore/models/HumanInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grocerystore.models;
+
+/**
+ *
+ * @author mac
+ */
+public interface HumanInterface {
+    void talk(String text);
+    
+    void buy(Product p);
+}
